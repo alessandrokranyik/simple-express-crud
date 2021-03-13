a5e9caaa1782cfe1aa108a1cd19da95520683efc
alessandrokranyik <alessandroj.kranyik@gmail.com>
2021-03-13 13:33:26 +0100

    Einfache CRUD Express api

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "bobby",
+    email: "bobby@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "dickson",
+    email: "dicky@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "swalley",
+    email: "swalley@gmail.com",
+    status: "offline",
+  },
+];
+
+module.exports = members;
